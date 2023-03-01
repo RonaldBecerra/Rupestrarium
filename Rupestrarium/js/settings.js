d7f7ca9583941408e4e204ee2ecff79f29c8de89
Ronald <12-10706@usb.ve>
2023-03-01 18:22:19 -0400

    Se movieron los parámetros de configuración a settings.js

---
+++ Rupestrarium/js/settings.js
@@ -0,0 +1,5 @@
+const smtpServerURL = "http://localhost:2526/mailServer";
+
+// If true, when finishing the quiz it will appear a view for sending the email results.
+// If false, it will directly appear the quiz results view.
+const SEND_EMAIL_IN_THIS_VERSION = false; 
